a47fab3b8cc243060e1d9b60c3261d106713ff89
Flavio Curella <flavio.curella@gmail.com>
2024-06-07 11:04:58 -0500

    add db.new_connection

--- django/db/utils.py
+++ django/db/utils.py
@@ -1,4 +1,5 @@
 import pkgutil
+from contextlib import asynccontextmanager
 from importlib import import_module
 
 from django.conf import settings
@@ -192,6 +193,18 @@ class ConnectionHandler(BaseConnectionHandler):
         db = self.settings[alias]
         backend = load_backend(db["ENGINE"])
         return backend.DatabaseWrapper(db, alias)
+
+    @asynccontextmanager
+    async def new_connection(self, using=DEFAULT_DB_ALIAS):
+        conn = self.create_connection(using)
+        token = self._async_connection.set(conn)
+        try:
+            yield conn
+        except NotSupportedError as exc:
+            raise NotSupportedError(f"Backend {conn} doesn't support async.") from exc
+        finally:
+            await conn.aclose()
+            self._async_connection.reset(token)
 
 
 class ConnectionRouter:
